08b1ea991d815410ba38685c38f1abc44afb29fa
jhoncharry <jhon.charryserrato@gmail.com>
2021-11-26 01:22:56 -0500

    Added same site

--- src/server.ts
+++ src/server.ts
@@ -35,6 +35,7 @@ async function init() {
       signed: false,
       // secure: process.env.NODE_ENV !== "test"  // ONLY FOR DEV ENVIRONMENT OR HTTPS CONNECTION (CHECK IF IT'S WORKING IN HTTPS CONNECTIONS)
       secure: process.env.NODE_ENV !== ("dev" || "test"), // IN ORDER TO DISABLE HTTPS checking, CASUE I HAVE ONLY HTTP AT THE MOMENT AND COOKIES WORKS WITH HTTPS
+      sameSite: "none",
     })
   );
 
